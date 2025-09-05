143c5a237bf5d77a3e9cb6719aefb59e80af3538
Guillaume Coquemont <gui.coquemont@gmail.com>
2025-09-05 12:19:58 +0200

    feat(prisma): add User model with Role enum and create admin/user in DB

---
+++ prisma/migrations/20250905101306_user_and_role/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "name" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "image" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "emailVerified" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
